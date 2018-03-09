92a946aca69f9dd320aeb9e91f65efa0b7014008
dearpengpeng <36717535+dearpengpeng@users.noreply.github.com>
2018-03-08 22:58:39 -0800

    Update Gallary.js

--- image display/Gallary.js
+++ image display/Gallary.js
@@ -1,13 +1,4 @@
-//
-//   var gallery = document.getElementById("imagegallery");
-//   var links = gallery.getElementsByTagName("a");
-//   for(var i=0; i<links.length;i++){
-//       links[i].onclick = function(){
-//       var source = this.getAttribute("src");
-//       var placeholder = document.getElementById("placeholder");
-//       placeholder.setAttribute("src",source);
-//       }
-//   } 
+
 
  window.onload = function(){
    function showPic(a){
